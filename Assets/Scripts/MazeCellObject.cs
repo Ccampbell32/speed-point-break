fcbfd3a70e9643ed2e50a6272be3ea529ef393af
Ccampbell32 <148860246+Ccampbell32@users.noreply.github.com>
2025-04-14 14:36:27 +0100

    maze generation

--- Assets/Scripts/MazeCellObject.cs
+++ Assets/Scripts/MazeCellObject.cs
@@ -1,6 +1,20 @@
+using System.Xml.Serialization;
 using UnityEngine;
 
-public class NewEmptyCSharpScript
+public class MazeCellObject : MonoBehaviour
 {
-    
+    [SerializeField] GameObject topWall;
+    [SerializeField] GameObject bottomWall;
+    [SerializeField] GameObject leftWall;
+    [SerializeField] GameObject rightWall;
+
+    public void Init (bool top, bool bottom, bool left, bool right)
+    {
+        topWall.SetActive (top);
+        bottomWall.SetActive (bottom);  
+        leftWall.SetActive (left);  
+            rightWall.SetActive (right);
+
+    }
+
 }
